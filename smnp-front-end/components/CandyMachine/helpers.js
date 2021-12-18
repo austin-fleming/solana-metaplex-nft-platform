bf434be825641e9599a267a952cfb575356c9f4f
flimflam-austin <68439674+flimflam-austin@users.noreply.github.com>
2021-12-17 17:57:16 -0800

    update example.env

--- smnp-front-end/components/CandyMachine/helpers.js
+++ smnp-front-end/components/CandyMachine/helpers.js
@@ -2,15 +2,15 @@ import { web3 } from '@project-serum/anchor';
 
 // CLI Properties Given to us
 const candyMachineProgram = new web3.PublicKey(
-  process.env.CANDY_MACHINE_PROGRAM
+  "cndyAnrLdpjq1Ssp1z8xxDsB8dxe7u4HL5Nxi2K5WXZ"
 );
 
 const TOKEN_METADATA_PROGRAM_ID = new web3.PublicKey(
-  process.env.TOKEN_METADATA_PROGRAM_ID
+  "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
 );
 
 const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID = new web3.PublicKey(
-  process.env.SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID
+  "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL"
 );
 
 export {
